37766eeb83572a6753ab697e51bbb4fce29c66be
unknown <SiarheySkuratovich@yandex.ru>
2018-12-11 20:42:41 +0300

    setOnSeekBarChangeListener

--- app/src/main/java/com/danielkim/soundrecorder/fragments/AudioSamplingSeekBarFragment.java
+++ app/src/main/java/com/danielkim/soundrecorder/fragments/AudioSamplingSeekBarFragment.java
@@ -7,6 +7,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.danielkim.soundrecorder.R;
 
@@ -18,5 +20,28 @@ public class AudioSamplingSeekBarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_audio_sampling_seekbar, container,false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SeekBar seekBar = (SeekBar)view.findViewById(R.id.sample_rate_seek_bar);
+        final TextView progress = (TextView) view.findViewById(R.id.progress);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                progress.setText("" + i + " Hz");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
